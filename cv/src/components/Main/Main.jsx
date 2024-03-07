078a15b99d52794151887cab9fc18df3ab57df55
Vitali Hoila <vgoylo91mav@gmail.com>
2024-03-07 17:05:41 +0300

    Add soft skills and changed main margen button

--- cv/src/components/Main/Main.jsx
+++ cv/src/components/Main/Main.jsx
@@ -6,7 +6,7 @@ export default function mainPart() {
     <main class="container border-bottom">
       <div class="container text-center my-3">
         <div class="row">
-          <div class="col-lg-6 col-md-3 mx-auto ">
+          <div class="col-lg-6 col-md-3 mx-auto">
             <img
               class={style.image}
               src="https://img.freepik.com/free-photo/creative-light-bulb-abstract-glowing-blue-background-generative-ai_188544-8090.jpg?size=626&ext=jpg&ga=GA1.1.1546980028.1708905600&semt=sph"
@@ -18,12 +18,16 @@ export default function mainPart() {
             <p>
               2 years of experience in Ruby on Rails and Frontend development.
             </p>
-            <a href="/skills" class="btn btn-primary">
-              PROFESSIONAL SKILLS
-            </a>
-            <a href="/projects" class="btn btn-secondary">
-              RECENT PROJECTS
-            </a>
+            <span class={style.button}>
+              <a href="/skills" class="btn btn-primary  my-3">
+                PROFESSIONAL SKILLS
+              </a>
+            </span>
+            <span class={style.button}>
+              <a href="/projects" class="btn btn-secondary">
+                RECENT PROJECTS
+              </a>
+            </span>
           </div>
         </div>
       </div>
--- cv/src/components/Skills/Skills.jsx
+++ cv/src/components/Skills/Skills.jsx
@@ -5,7 +5,7 @@ export default function Skills() {
   return (
     <main class="container border-bottom">
       <div class="row">
-        <div class="col-lg-6 col-md-3 mx-auto my-3">
+        <div class="col-lg-7 col-md-5 mx-auto my-3">
           <h2 class="text-center">PROFESSIONAL SKILLS</h2>
           <ul class="blockquote text-start">
             <li>Programming languages: Ruby, Javascript, SQL.</li>
@@ -26,31 +26,39 @@ export default function Skills() {
             <li>Operating systems: Linux (Ubuntu), Mac OS, Windows.</li>
           </ul>
         </div>
-        <div class="row">
-          <div class="col-lg-12  mx-auto">
-            <div class={style.position}>
-              <h2>EXPERIENCE</h2>
-              <ul class="blockquote text-start">
-                <li>iTechArt, Ruby on Rails Developer</li>
-                <li>2021 - present</li>
-              </ul>
-            </div>
-
-            <div class={style.position}>
-              <h2>LANGUAGES</h2>
-              <ul class="blockquote text-start">
-                <li>Intermediate: English.</li>
-                <li>Native: Belarusian, Russian</li>
-              </ul>
-            </div>
-            <div class={style.position}>
-              <h2>EDUCATION</h2>
-              <ul class="blockquote text-start">
-                <li>2008-2011</li>
-                <li>Belarusian State Academy of Aviation</li>
-                <li>Faculty of Radio electronic equipment.</li>
-              </ul>
-            </div>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
+          <div class="col">
+            <h2>EXPERIENCE</h2>
+            <ul class="blockquote text-start">
+              <li>iTechArt, Ruby on Rails Developer</li>
+              <li>2021 - present</li>
+            </ul>
+          </div>
+          <div class="col">
+            <h2>LANGUAGES</h2>
+            <ul class="blockquote text-start">
+              <li>Intermediate: English.</li>
+              <li>Native: Belarusian, Russian</li>
+            </ul>
+          </div>
+          <div class="col">
+            <h2>EDUCATION</h2>
+            <ul class="blockquote text-start">
+              <li>2008-2011</li>
+              <li>Belarusian State Academy of Aviation</li>
+              <li>Faculty of Radio electronic equipment.</li>
+            </ul>
+          </div>
+          <div class="col">
+            <h2>SOFT SKILLS</h2>
+            <ul class="blockquote text-start">
+              <li>Time management</li>
+              <li>Problem-solving</li>
+              <li>Multitasking</li>
+              <li>
+                Ability to communicate well, actively listen and responsible
+              </li>
+            </ul>
           </div>
         </div>
       </div>
